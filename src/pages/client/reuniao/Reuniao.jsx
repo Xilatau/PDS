9442adf088c02e3d8d73e1a7222116dd9faa66db
Miguel Pires <zemiguel.pires@hotmail.com>
2025-05-28 12:51:55 +0100

    fix pedidos reuniao

--- src/pages/client/reuniao/Reuniao.jsx
+++ src/pages/client/reuniao/Reuniao.jsx
@@ -33,45 +33,32 @@ export default function PostModal({ onClose }) {
     <div className="modal-backdrop">
       <div className="modal">
         <h2>Pedir Reunião</h2>
-        <form className="form-post" onSubmit={handleSubmit}>
+        
+        <form className="form-reuniao" onSubmit={handleSubmit}> 
+          <div className="reuniao-row">
+            <div>
+              <label>Data da reunião:</label>
+              <input type="date" value={title} onChange={e => setTitle(e.target.value)} required />
+            </div>
+            <div>
+              <label>Hora da reunião:</label>
+              <input type="time" value={time} onChange={e => setTime(e.target.value)} required />
+            </div>
+          </div>
+            
+          <div className="reuniao-description">
+            <label className="label-message">Motivo da reunião:</label>
+            <textarea rows="7" value={message} onChange={e => setMessage(e.target.value)} required/>
+          </div>
           
-          <label className="label-title">
-            Data da reunião:
-            <input
-              type="date"
-              value={title}
-              onChange={e => setTitle(e.target.value)}
-              required
-            />
-          </label>
-
-          <label className="label-title">
-            Hora da reunião:
-            <input
-              type="time"
-              value={time}
-              onChange={e => setTime(e.target.value)}
-              required
-            />
-          </label>
-
-          <label className="label-message">
-            <div>Motivo da reunião:</div>
-            <textarea
-              rows="7"
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-              required
-            />
-          </label>
-
-          <div className="modal-actions">
-            <button  className='cancel' type="button" onClick={onClose}>Cancelar</button>
-            <button type="submit" className="primary">Enviar</button>
+          <div className="modal-buttons-reuniao">
+            <button className='btn-cancel' type="button" onClick={onClose}>Cancelar</button>
+            <button className="btn-submit" type="submit">Enviar</button>
           </div>
         </form>
+
       </div>
-      <button className="v" onClick={() => navigate('/MinhasReunioes')}>Ver Pedidos</button>
+      <button className="verpedidos" onClick={() => navigate('/MinhasReunioes')}>Ver Pedidos</button>
     </div>
   );
 }
